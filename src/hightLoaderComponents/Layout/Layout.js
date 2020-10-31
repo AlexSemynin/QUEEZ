259b567b50f4d85b4c3a011d3a1a3bb92027fcf2
Alex Semynin <alex.semynin1997@outlook.com>
2020-10-31 21:37:10 +0300

    Добавить модульность в css. В обычном проекте ковыряй webpack css loader для этого

--- src/hightLoaderComponents/Layout/Layout.js
+++ src/hightLoaderComponents/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import classes from './Layout.module.css'
 
 
 class Layout extends React.Component{
     render(){
         return(
-            <div>
+            <div className={classes.Layout}>
 
 
                 <main>
