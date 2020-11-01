8d339e1dce7fabd08886a3d2ceec175576c50eaf
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-01 19:10:56 +0300

    Стилизовать компонент Окончание теста

--- src/components/FinisedQuiz/FinisedQuiz.js
+++ src/components/FinisedQuiz/FinisedQuiz.js
@@ -3,8 +3,22 @@ import classes from './FinisedQuiz.module.css'
 
 const FinisedQuiz = (props) => {
     return(
-        <div>
-            <h1>FinisedQuiz</h1>
+        <div className={classes.FinisedQuiz}>
+            <ul>
+                <li>
+                    <strong>1.&nbsp;</strong>
+                    какого цвета?
+                    <i className={'fa fa-times ' + classes.error}></i>
+                </li>
+                <li>
+                <strong>2.&nbsp;</strong>
+                    в каком гожу?
+                    <i className={'fa fa-check success ' + classes.success}></i>
+                </li>
+            </ul>
+
+            <p>правильно 1 из 2</p>
+            <button>Повторить</button>
         </div>
     )
 }
