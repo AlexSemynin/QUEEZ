cd2097f71c4e42639c48b46b2fb178022a223204
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-01 21:22:21 +0300

    Отобразать результат ответов в UI в конечном компоненте

--- src/components/FinisedQuiz/FinisedQuiz.js
+++ src/components/FinisedQuiz/FinisedQuiz.js
@@ -2,22 +2,36 @@ import React from 'react';
 import classes from './FinisedQuiz.module.css'
 
 const FinisedQuiz = (props) => {
+
+    const trueAnswersCount = props.results.filter(result => result.result === "success").length;
     return(
         <div className={classes.FinisedQuiz}>
             <ul>
-                <li>
-                    <strong>1.&nbsp;</strong>
-                    какого цвета?
-                    <i className={'fa fa-times ' + classes.error}></i>
-                </li>
-                <li>
-                <strong>2.&nbsp;</strong>
-                    в каком гожу?
-                    <i className={'fa fa-check success ' + classes.success}></i>
-                </li>
-            </ul>
+            {
+                props.results.map((resulModel, index) => {
 
-            <p>правильно 1 из 2</p>
+                    const faIcon = resulModel.result === "success" ?  "fa-check" : "fa-times";
+                    const cls = [
+                        'fa',
+                        faIcon,
+                        classes[resulModel.result]
+                    ];
+
+                    return(
+                        <li key={index}>
+                            <span>
+                                <strong>{resulModel.key}.&nbsp;</strong>
+                                {props.quizs[resulModel.key].question}
+                                <i className={cls.join(" ")}></i>
+                            </span>
+
+                        </li>
+                    )
+                })
+            }
+            </ul>
+            
+            <p>правильно {trueAnswersCount} из {props.results.length}</p>
             <button>Повторить</button>
         </div>
     )
