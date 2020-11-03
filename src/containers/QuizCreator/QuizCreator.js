dabbd183af59eb087f3b6c3a2c5453c9c81406d6
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-03 21:33:13 +0300

    Добавить контейнеры в проект (class)

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Auth extends React.Component{
+
+    render(){
+
+        return(
+
+            <div>
+                <h1>Quiz Creator</h1>
+            </div>
+        )
+    }
+}
