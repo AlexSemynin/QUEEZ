635468fa7e9d3c5c56d67de7fa188a8ca0db30a3
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-04 00:23:04 +0300

    Создать форму Авторизации

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
 
 export default class Auth extends React.Component{
+
+
+    submitAuth = ev =>{
+        ev.preventDefault();
+    }
+
+    loginHandler = ()=>{
+
+    }
+    registerHandler = ()=>{
+        
+    }
 
     render(){
 
         return(
 
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.submitAuth}>
+                        <input type='text'/>
+                        <input type='text'/> 
+                        <div className="wrapperButton">
+                            <Button
+                                type="success"
+                                onClick={this.loginHandler}
+                            >Авторизоваться</Button>
+                            <Button
+                                type="primary"
+                                onClick={this.registerHandler}
+                            >Зарегистрироваться</Button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
