2f0780bdc982e0bca7b07dae97cf03be6e276b54
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-03 22:15:57 +0300

    Сделать навигацию для списка тестов

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './QuizList.module.css';
 
 export default class QuizList extends React.Component{
+
+
+    renderQuizes = ()=>{
+
+        return [1,2,3].map((quiz,index) => {
+            return(
+                <li key={index}>
+                    <NavLink 
+                        to={"/quiz/"+quiz}
+                    >
+                        quiz {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
 
     render(){
 
         return(
 
-            <div>
-                <h1>QuizList</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
         )
     }
