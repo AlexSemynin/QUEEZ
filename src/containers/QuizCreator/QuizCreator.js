9d0bcbe1cdc1c8a5e437c51ca7bd5345e3ad4ee4
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-04 12:15:22 +0300

    стилизовать компонент Создания Quiz-теста

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+import classes from './QuizCreator.module.css'
 
-export default class Auth extends React.Component{
+export default class QuizCreator extends React.Component{
+
+    //todo:Доделать логику компонента: начать с state и добавлением в него и на страницу вопросов
+
+    state = {
+        quizs: [], //{questionId:int, question: string, answers:[{text:'', ansId:1},...], tryAnswerId: int}
+        currentFormQuiz:[
+            {}
+        ]
+    }
+
+    onSubmitHandler = (ev) => {
+        ev.preventDefault();
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    renderQuestion = () => {
+        return(
+            <div className="questionWrapper">
+                <Input label='Вопрос'/>
+                <hr/>
+                <span><strong>Варианты ответов:</strong></span>
+                <Input/>
+                <Input/>
+                <Input/>
+                <Input/>
+                <Input inputType='number' label='Правильный ответ'/>
+            </div>
+        )
+    }
+
+
+
+
 
     render(){
 
         return(
 
-            <div>
-                <h1>Quiz Creator</h1>
+            <div className={classes.QuizCreator}>
+                <h1>Создание теста</h1>
+
+                <form onSubmit={this.onSubmitHandler}>
+                    {/* {this.addQuestionHandler()} */}
+                    <div className={classes.buttonsWrapper}>
+                        <Button type="primary" onClick={this.addQuestionHandler}>Добавить вопрос</Button>
+                        <Button type="success">Создать тест</Button>
+                    </div>
+                </form>
             </div>
         )
     }
