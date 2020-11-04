4b08d93711e75284301b45b8f09522d0f3ed9b28
Alex Semynin <alex.semynin1997@outlook.com>
2020-11-04 10:17:35 +0300

    ВАЛИДАЦИЯ part2: Реализовать валидацию на inputs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -56,7 +56,7 @@ export default class Auth extends React.Component{
                     inputType={control.type}
                     label={control.label}
                     invalidText={control.errorMessage}
-                    valid={control.errorMessage}
+                    valid={control.valid}
                     toched={control.toched}
                     shouldValidate={!!control.validation}
                     onChange={(ev) => this.onChangeHandler(ev,controlName)}
@@ -66,11 +66,34 @@ export default class Auth extends React.Component{
     }
 
     onChangeHandler = (ev, controlName) => {
-        console.log(`${controlName}: ${ev.target.value}`);
-
+        const formControls = {...this.state.formControls}; // копировать контролы для дальнейшего их изменения easy 
+        const control = formControls[controlName];
+        control.toched = true;
+        control.value = ev.target.value;
+        control.valid = this.IsValidControl(ev.target.value, control.validation);
+        
         this.setState({
-            
+            formControls: formControls
         })
+    }
+
+    IsValidControl(value, validation){
+        if(!validation) return true; //если кнотрол не нужно валидировать
+
+        let isValid = true; 
+
+        if(validation.requied){
+            isValid = value.trim() !== "";
+        }
+        if(validation.email){
+            const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            isValid = re.test(String(value).toLowerCase());
+        }
+        if(validation.minLength){
+            isValid = value.length >= validation.minLength;
+        }
+
+        return isValid;
     }
 
     render(){
